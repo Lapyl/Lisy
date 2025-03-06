43ee34a178442ac267ad27cec408065f2f9e639d
Lalit A Patel <Lapyl@users.noreply.github.com>
2025-03-06 11:46:43 -0800

    Revised lisy.go and lisy.exe
    Revised lisy.go and lisy.exe

--- README.md
+++ README.md
@@ -1,74 +1,19 @@
-## Project
+## LISY
 
-Lisy stands for 'List Insp Sync Ying'. It is a Windows desktop app, developed by Lalit A Patel and Laxmi L Patel, and submitted to Digital Jam Hackathon 2025.
+LISY is a Windows desktop app for listing, backing-up, and viewing a folder.
 
-## Files on this Repository
+## Files
 
 - lisy.go : Go code
 - Lisy.exe : Windows executable file built by using lisy.go
+- go.mod and go.sum : Files generated upon running 'go mod tidy'
 
-## Links
+## Video
 
-- Project Submission on DevPost: https://devpost.com/software/lisy-windows-desktop-app-for-files-management
-- Project Video on YouTube: https://youtu.be/4db3sfWrPSM
-- GitHub repository: https://github.com/Lapyl/Lisy
-- Hackathon Information on Devpost: https://digital-jam.devpost.com/
+- YouTube video: https://youtu.be/vNfyyB-1BtI
 
-## Description on DevPost
+## To Do
 
-- Inspiration
-- Keeping an inventory of digital files on computers is of paramount importance. We often like to have a quick preview and inspection of pictures stored in a folder. All of us routinely need to create back up replicas of drives or folders.
-- What it does
-- Lisy’s List module allows us to prepare lists of contents in a selected folder. Lisy’s Insp module allows us to slide through files in a folder. Lisy’s Sync module can be used for to creating back up replicas of drives or folders. Lisy’ Ying is for a little fun.
-- How we built it
-- Lisy has been written using Google’s Go programming language, which is efficient and easy to use. After reviewing pros and cons of various GUI approaches, we have used Gioui package and some Html approach for providing a user-friendly graphical user interface. 
-- Challenges we ran into
-- We found that Gioui package is not so good as Fyne package for canvases with images.
-- Accomplishments that we're proud of
-- Windows executable file Lisy.exe that we have built can be immensely useful in our and others' routine tasks.  
-- What we learned
-- We have sharpened our skills in Go programming languages. We now understand different GUI approaches available for Go.
-- What's next for Lisy
-- We will enhance the code to: Allow Lisy List and Sync modules to remember and reuse users' inputs.  Allow Insp module to show pictures on an application window rather than as a wallpaper and to change the speed of sliding.
-
-## Narration in video
-
-- Hi, I am Lalit Patel.
-- And, I am Laxmi Patel.
-- We have developed a Windows desktop app named Lisy.
-- Keeping an inventory of digital files on computers is of paramount importance.
-- Lisy’s List module allows us to prepare lists of contents in a selected folder.
-- We often like to have a quick preview and inspection of pictures stored in a folder.
-- Lisy’s Insp module allows us to slide through files in a folder.
-- All of us routinely need to create back up replicas of drives or folders. 
-- Lisy’s Sync module can be used for that purpose.
-- How about some fun?
-- Lisy’ Ying can help.
-- Lisy has been written using Google’s Go programming language.
-- Go is efficient, and easy to use.
-- Here is the main Go code used for Lisy.
-- ‘Go Mod Tidy’ command brings in necessary libraries.
-- ‘Go Run Dot’ command runs the code while developing.
-- ‘Go Build’ compiles and builds a Windows executable file.
-- Lisy’s application window is simple.
-- Top part has 4 buttons for the 4 modules.
-- Below that is a text box for writing inputs.
-- Then there is a progress bar and an image block. 
-- For the List part, you specify your folder’s path.
-- Clicking List quickly prepares two csv files, for subfolders and files.
-- Each list has item name, file size, and modified date.
-- For the Insp part too, you specify a folder’s path.
-- Clicking Insp presents pictures one by one. 
-- Future versions of Lisy will allow us to configure how pictures are presented.
-- The Sync part needs paths of source and target folders.  
-- Clicking Sync backs up the source folder to the target folder. 
-- Sync uses Robocopy and is pretty fast.
-- The Ying part needs you to specify seconds for heating the rock picture.
-- Clicking Ying changes size and color of the image.
-- It can be a little fun to watch this. 
-- Thank you.
-- Thank you.
-
-## Acknowledgement
-
-The Go code of Lisy’s Ying part is based on https://jonegil.github.io/gui-with-gio/ .
+- Enable Lisy to accept arguments when used through a command prompt.
+- Allow Lisy's Sync modules to remember and reuse users' inputs.
+- Make Lisy's View part to close silently.
--- lisy.go
+++ lisy.go
@@ -8,7 +8,6 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 	"log"
-	"math"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -17,32 +16,231 @@ import (
 	"time"
 
 	"gioui.org/app"
-	"gioui.org/f32"
 	"gioui.org/layout"
 	"gioui.org/op"
-	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/text"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
-	"github.com/ajstarks/giocanvas"
 )
 
 var zcha chan string
-var zpri chan bool
 var zind int
-var zmsg string
-var zlrd string
-var zlrf string
+var zpat string
+var zpaa []string
+
+type ImageResult struct {
+	Error  error
+	Format string
+	Image  image.Image
+}
 
 func main() {
 	go func() {
 		fmt.Println("A")
 		w1 := new(app.Window)
 		w1.Option(app.Title("LISY"))
-		w1.Option(app.Size(unit.Dp(400), unit.Dp(600)))
-		if err := abc4(w1); err != nil {
+		w1.Option(app.Size(unit.Dp(600), unit.Dp(600)))
+		if err := abc0(w1); err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
+
+func abc0(w *app.Window) error {
+	var xsou widget.Editor
+	var xdes widget.Editor
+	var xlst widget.Clickable
+	var xsyn widget.Clickable
+	var xvue widget.Clickable
+	var xmsg widget.Editor
+	var zsou string
+	var zdes string
+	zmsg := "LIST prepares csv files of lists of folders and files in your specified source folder."
+	zmsg = zmsg + "\nSYNC synchronizes your specified backup folder with your specified source folder."
+	zmsg = zmsg + "\nSHOW presents a slideshow of images from your specified source folder and then freeze this window."
+	var zops op.Ops
+	th := material.NewTheme()
+	for {
+		switch e := w.Event().(type) {
+		case app.FrameEvent:
+			gtx := app.NewContext(&zops, e)
+			if xlst.Clicked(gtx) {
+				zlin := ""
+				zlrd := ""
+				zlrf := ""
+				ztag := time.Now().Format("06-01-02_15-04")
+				ztop := strings.TrimSpace(xsou.Text())
+				err := filepath.Walk(ztop,
+					func(path string, info os.FileInfo, err error) error {
+						if err != nil {
+							return err
+						}
+						zlin = strings.Replace(path, ztop, "", -1) + "," + strconv.Itoa(int(info.Size())) + "," + info.ModTime().Format("06-01-02_15-04")
+						fmt.Println(zlin)
+						if info.IsDir() {
+							zlrd = zlrd + zlin + "\r\n"
+						} else {
+							zlrf = zlrf + zlin + "\r\n"
+						}
+						return nil
+					})
+				if err != nil {
+					log.Println(err)
+				} else {
+					os.WriteFile(ztop+"\\ListDir_"+ztag+".csv", []byte("RelPath,Bytes,ModYmdhm\r\n"+zlrd), 0666)
+					os.WriteFile(ztop+"\\ListFil_"+ztag+".csv", []byte("RelPath,Bytes,ModYmdhm\r\n"+zlrf), 0666)
+				}
+				zmsg = "See: " + ztop + "\\ListDir(Fil)_" + ztag + ".csv"
+				fmt.Println(zmsg)
+				zmsg = zmsg + "\n" + zlrf
+			}
+			if xvue.Clicked(gtx) {
+				abc1(strings.TrimSpace(xsou.Text()))
+			}
+			if xsyn.Clicked(gtx) {
+				zsou = strings.TrimSpace(xsou.Text())
+				zdes = strings.TrimSpace(xdes.Text())
+				if len(xdes.Text()) > 0 {
+					zcmd := exec.Command("ROBOCOPY", zsou, zdes, "/MIR")
+					zout, err := zcmd.CombinedOutput()
+					if err != nil {
+						zmsg = "Error: " + string(err.Error())
+					} else {
+						zmsg = string(zout)
+					}
+					fmt.Println(zmsg)
+				}
+				xmsg.SetText(zmsg)
+			}
+			layout.Flex{
+				Axis:    layout.Vertical,
+				Spacing: layout.SpaceStart,
+			}.Layout(
+				gtx,
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return material.Label(th, unit.Sp(10), "Specify your source folder, for List or Sync or View.").Layout(gtx)
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					ed := material.Editor(th, &xsou, "Source folder (for List Sync View)")
+					xsou.SingleLine = false
+					xsou.Alignment = text.Start
+					margins := layout.Inset{
+						Top:    unit.Dp(1),
+						Bottom: unit.Dp(1),
+						Left:   unit.Dp(10),
+						Right:  unit.Dp(10),
+					}
+					border := widget.Border{
+						Color:        color.NRGBA{R: 204, G: 204, B: 204, A: 255},
+						CornerRadius: unit.Dp(5),
+						Width:        unit.Dp(1),
+					}
+					return margins.Layout(gtx,
+						func(gtx layout.Context) layout.Dimensions {
+							return border.Layout(gtx, ed.Layout)
+						},
+					)
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return material.Label(th, unit.Sp(10), "Specify your backup folder, for Sync.").Layout(gtx)
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					ed := material.Editor(th, &xdes, "Backup folder (for Sync)")
+					xdes.SingleLine = false
+					xdes.Alignment = text.Start
+					margins := layout.Inset{
+						Top:    unit.Dp(1),
+						Bottom: unit.Dp(1),
+						Left:   unit.Dp(10),
+						Right:  unit.Dp(10),
+					}
+					border := widget.Border{
+						Color:        color.NRGBA{R: 204, G: 204, B: 204, A: 255},
+						CornerRadius: unit.Dp(5),
+						Width:        unit.Dp(1),
+					}
+					return margins.Layout(gtx,
+						func(gtx layout.Context) layout.Dimensions {
+							return border.Layout(gtx, ed.Layout)
+						},
+					)
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return material.Label(th, unit.Sp(10), "Click List or Sync or View button.").Layout(gtx)
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{}.Layout(gtx,
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							return material.Button(th, &xlst, "LIST").Layout(gtx)
+						}),
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							return material.Button(th, &xsyn, "SYNC").Layout(gtx)
+						}),
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							return material.Button(th, &xvue, "SHOW").Layout(gtx)
+						}),
+					)
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return material.Label(th, unit.Sp(10), "Note the following.").Layout(gtx)
+				}),
+				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+					ed := material.Editor(th, &xmsg, zmsg)
+					xmsg.SingleLine = false
+					xmsg.Alignment = text.Start
+					margins := layout.Inset{
+						Top:    unit.Dp(1),
+						Bottom: unit.Dp(1),
+						Left:   unit.Dp(10),
+						Right:  unit.Dp(10),
+					}
+					border := widget.Border{
+						Color:        color.NRGBA{R: 204, G: 204, B: 204, A: 255},
+						CornerRadius: unit.Dp(5),
+						Width:        unit.Dp(1),
+					}
+					return margins.Layout(gtx,
+						func(gtx layout.Context) layout.Dimensions {
+							return border.Layout(gtx, ed.Layout)
+						},
+					)
+				}),
+			)
+			e.Frame(gtx.Ops)
+		case app.DestroyEvent:
+			return e.Err
+		}
+	}
+}
+
+func abc1(ztop string) {
+	zpat = ""
+	filepath.Walk(ztop,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			} else {
+				if info.IsDir() {
+					fmt.Println("Dir: " + path)
+				} else {
+					if strings.HasSuffix(path, ".jpg") || strings.HasSuffix(path, ".png") {
+						fmt.Println("File: " + path)
+						zpat = zpat + "," + path
+					}
+				}
+			}
+			return nil
+		})
+	zpaa = strings.Split(zpat, ",")
+	fmt.Println(len(zpaa))
+	go func() {
+		w2 := new(app.Window)
+		w2.Option(app.Title("LisyImages"))
+		if err := abc2(w2); err != nil {
 			log.Fatal(err)
 		}
 		os.Exit(0)
@@ -51,298 +249,56 @@ func main() {
 	zind = 0
 	go func() {
 		for {
-			time.Sleep(time.Second * 1)
-			zcha <- "Lisy"
-		}
-	}()
-	zpri = make(chan bool, 2)
-	go func() {
-		for {
-			time.Sleep(time.Second / 25)
-			zpri <- true
+			if zind < len(zpaa)-2 {
+				time.Sleep(time.Second * 2)
+				zcha <- "LisyImages"
+			}
 		}
 	}()
 	app.Main()
 }
 
-func abc1(ztop string) {
-	ztag := time.Now().Format("06-01-02_15-04")
-	zlrd = ""
-	zlrf = ""
-	fmt.Println("==========================================================")
-	fmt.Println(ztop)
-	err := filepath.Walk(ztop,
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			zlin := strings.Replace(path, ztop, "", -1) + "," + strconv.Itoa(int(info.Size())) + "," + info.ModTime().Format("06-01-02_15-04")
-			fmt.Println(zlin)
-			if info.IsDir() {
-				zlrd = zlrd + zlin + "\r\n"
-			} else {
-				zlrf = zlrf + zlin + "\r\n"
-			}
-			return nil
-		})
-	if err != nil {
-		log.Println(err)
-	} else {
-		os.WriteFile(ztop+"\\ListDir_"+ztag+".csv", []byte("RelPath,Bytes,ModYmdhm\r\n"+zlrd), 0666)
-		os.WriteFile(ztop+"\\ListFil_"+ztag+".csv", []byte("RelPath,Bytes,ModYmdhm\r\n"+zlrf), 0666)
-	}
-	zmsg = "See: " + ztop + "\\ListDir(Fil)_" + ztag + ".csv"
-	fmt.Println(zmsg)
-}
-
-func abc2(s string) (image.Image, error) {
-	i, err := os.Open(s)
-	if err != nil {
-		return nil, err
-	}
-	im, _, err := image.Decode(i)
-	if err != nil {
-		return nil, err
-	}
-	i.Close()
-	return im, nil
-}
-
-func abc3(w *app.Window, s string) error {
-	var zops op.Ops
-	material.NewTheme()
-	bgcolor := color.NRGBA{100, 100, 100, 255}
+func abc2(w *app.Window) error {
+	var ops op.Ops
+	th := material.NewTheme()
+	var openBtn widget.Clickable
 	for {
 		switch e := w.Event().(type) {
 		case app.FrameEvent:
 			select {
 			case ztit := <-zcha:
-				w.Option(app.Title(ztit))
-				zind = zind + 1
-				if zind > 40 {
-					w.Close()
+				if zind > len(zpaa)-1 {
+					// w.Close()
 					return nil
-				}
-				im, err := abc2(strings.Split(s, ",")[zind])
-				if err != nil {
-					return err
-				}
-				fmt.Println(zind)
-				gtx := app.NewContext(&zops, e)
-				layout.Flex{
-					Axis:    layout.Vertical,
-					Spacing: layout.SpaceStart,
-				}.Layout(
-					gtx,
-					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-						canvas := giocanvas.NewCanvas(float32(e.Size.X), float32(e.Size.Y), app.FrameEvent{})
-						canvas.Background(bgcolor)
-						canvas.Grid(0, 0, 100, 100, 0.1, 10, color.NRGBA{128, 128, 128, 255})
-						canvas.Img(im, 100, 0, 600, 400, 100)
-						e.Frame(canvas.Context.Ops)
-						return layout.Dimensions{Size: image.Point{Y: 100}}
-					},
-					),
-				)
-			}
-		case app.DestroyEvent:
-			w.Close()
-			return e.Err
-		}
-	}
-}
-
-func abc4(w *app.Window) error {
-	var zbt1 widget.Clickable
-	var zbt2 widget.Clickable
-	var zbt3 widget.Clickable
-	var zbt4 widget.Clickable
-	var zibx widget.Editor
-	var ztrk bool
-	var znum float32
-	var zpro float32
-	var zops op.Ops
-	th := material.NewTheme()
-	go func() {
-		for range zpri {
-			if ztrk && zpro < 1 {
-				zpro += 1.0 / 25.0 / znum
-				if zpro >= 1 {
-					zpro = 1
-				}
-				w.Invalidate()
-			}
-		}
-	}()
-	for {
-		switch e := w.Event().(type) {
-		case app.FrameEvent:
-			gtx := app.NewContext(&zops, e)
-			if zbt1.Clicked(gtx) {
-				fmt.Println("List")
-				zist := zibx.Text()
-				zist = strings.TrimSpace(zist)
-				abc1(zist)
-				zibx.SetText(zmsg)
-			}
-			if zbt2.Clicked(gtx) {
-				fmt.Println("Insp")
-				ztop := zibx.Text()
-				zpat := ""
-				w2 := new(app.Window)
-				w2.Option(app.Title("Insp"))
-				filepath.Walk(ztop,
-					func(path string, info os.FileInfo, err error) error {
-						if err != nil {
-							return err
-						} else {
-							if info.IsDir() {
-								fmt.Println("Dir: " + path)
-							} else {
-								if strings.HasSuffix(path, ".jpg") || strings.HasSuffix(path, ".png") {
-									fmt.Println("File: " + path)
-									zpat = zpat + "," + path
-								}
-							}
-						}
-						return nil
-					})
-				abc3(w2, zpat)
-			}
-			if zbt3.Clicked(gtx) {
-				fmt.Println("Sync")
-				zist := zibx.Text()
-				zist = strings.TrimSpace(zist)
-				zina := strings.Split(zist, "\n")
-				abc1(zina[0])
-				zibx.SetText(zmsg)
-				abc1(zina[1])
-				zibx.SetText(zmsg)
-				zcmd := exec.Command("ROBOCOPY", zina[0], zina[1], "/MIR")
-				out, err := zcmd.CombinedOutput()
-				if err != nil {
-					fmt.Println("Error: ", err.Error())
-					zibx.SetText("Error: " + err.Error())
 				} else {
-					fmt.Println(out)
-					zibx.SetText(string(out))
-				}
-			}
-			if zbt4.Clicked(gtx) {
-				fmt.Println("Ying")
-				ztrk = !ztrk
-				if zpro >= 1 {
-					zpro = 0
-				}
-				zist := zibx.Text()
-				zist = strings.TrimSpace(zist)
-				zinu, _ := strconv.ParseFloat(zist, 32)
-				znum = float32(zinu)
-				znum = znum / (1 - zpro)
-			}
-			layout.Flex{
-				Axis:    layout.Vertical,
-				Spacing: layout.SpaceStart,
-			}.Layout(
-				gtx,
-				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-					return layout.Flex{}.Layout(gtx,
-						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-							return layout.N.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-								return layout.UniformInset(8).Layout(gtx,
-									material.Button(th, &zbt1, "List").Layout,
-								)
-							})
+					zind = zind + 1
+					fmt.Println(zind)
+					gtx := app.NewContext(&ops, e)
+					w.Option(app.Title(ztit))
+					file, err := os.OpenFile(strings.Split(zpat, ",")[zind], 0, 0)
+					if err != nil {
+						err = fmt.Errorf("failed opening image file: %w", err)
+					}
+					defer file.Close()
+					imgData, format, err := image.Decode(file)
+					if err != nil {
+						err = fmt.Errorf("failed decoding image data: %w", err)
+					}
+					img := ImageResult{Image: imgData, Format: format}
+					layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+						layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+							return material.Button(th, &openBtn, zpaa[zind]).Layout(gtx)
 						}),
 						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-							return layout.N.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-								return layout.UniformInset(8).Layout(gtx,
-									material.Button(th, &zbt2, "Insp").Layout,
-								)
-							})
-						}),
-						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-							return layout.N.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-								return layout.UniformInset(8).Layout(gtx,
-									material.Button(th, &zbt3, "Sync").Layout,
-								)
-							})
-						}),
-						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-							return layout.N.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-								return layout.UniformInset(8).Layout(gtx,
-									material.Button(th, &zbt4, "Ying").Layout,
-								)
-							})
+							return widget.Image{
+								Src: paint.NewImageOp(img.Image),
+								Fit: widget.Contain,
+							}.Layout(gtx)
 						}),
 					)
-				}),
-				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-					ed := material.Editor(th, &zibx, "List: Top folder.\nInsp: Images folder.\nSync: Source & target.\nYing: Limit second")
-					zibx.SingleLine = false
-					zibx.Alignment = text.Middle
-					if ztrk && zpro < 1 {
-						zrem := (1 - zpro) * znum
-						zstr := fmt.Sprintf("%.1f", math.Round(float64(zrem)*10)/10)
-						zibx.SetText(zstr)
-					}
-					margins := layout.Inset{
-						Top:    unit.Dp(0),
-						Right:  unit.Dp(10),
-						Bottom: unit.Dp(0),
-						Left:   unit.Dp(10),
-					}
-					border := widget.Border{
-						Color:        color.NRGBA{R: 204, G: 204, B: 204, A: 255},
-						CornerRadius: unit.Dp(3),
-						Width:        unit.Dp(2),
-					}
-					return margins.Layout(gtx,
-						func(gtx layout.Context) layout.Dimensions {
-							return border.Layout(gtx, ed.Layout)
-						},
-					)
-				},
-				),
-				layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-					bar := material.ProgressBar(th, zpro)
-					return bar.Layout(gtx)
-				},
-				),
-				layout.Rigid(
-					func(gtx layout.Context) layout.Dimensions {
-						var zimg clip.Path
-						op.Offset(image.Pt(gtx.Dp(200), gtx.Dp(190))).Add(gtx.Ops)
-						zimg.Begin(gtx.Ops)
-						for deg := 0.0; deg <= 360; deg++ {
-							rad := deg / 360 * 2 * math.Pi
-							cosT := math.Cos(rad)
-							sinT := math.Sin(rad)
-							a := uint8(75.0 * (2 - zpro))
-							b := uint8(100.0 * (2 - zpro))
-							d := 20.0
-							x := float64(a) * cosT
-							y := -(math.Sqrt(float64(b)*float64(b)-d*d*cosT*cosT) + d*sinT) * sinT
-							p := f32.Pt(float32(x), float32(y))
-							zimg.LineTo(p)
-						}
-						zimg.Close()
-						zare := clip.Outline{Path: zimg.End()}.Op()
-						color := color.NRGBA{
-							R: 255,
-							G: uint8(239 * (1 - zpro)),
-							B: uint8(174 * (1 - zpro)),
-							A: 255,
-						}
-						paint.FillShape(gtx.Ops, color, zare)
-						d := image.Point{Y: 375}
-						return layout.Dimensions{Size: d}
-					},
-				),
-			)
-			e.Frame(gtx.Ops)
-		case app.DestroyEvent:
-			return e.Err
+					e.Frame(gtx.Ops)
+				}
+			}
 		}
 	}
 }
